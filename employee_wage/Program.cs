1d4917e61224cc7827a48b7a77dc0fdc01f2be49
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-09-03 22:03:11 +0530

    wage of employee

--- employee_wage/Program.cs
+++ employee_wage/Program.cs
@@ -8,7 +8,6 @@ namespace employeeWage
         {
             //Contant
             int IS_FULL_TIME = 1;
-            int IS_PART_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
             //Variable
             int empHrs = 0;
@@ -17,11 +16,7 @@ namespace employeeWage
             Random random = new Random();
             //Computation
             int empCheck = random.Next(0, 3);
-            if (empCheck == IS_PART_TIME)
-            {
-                empHrs = 8;
-            }
-            else if (empCheck == IS_FULL_TIME)
+             if (empCheck == IS_FULL_TIME)
             {
                 empHrs = 8;
             }
