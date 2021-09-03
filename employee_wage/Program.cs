f4b905678b628ce65339533e195c587ba2ebd111
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-09-03 21:58:25 +0530

    part time wage of employee

--- employee_wage/Program.cs
+++ employee_wage/Program.cs
@@ -1,24 +1,36 @@
 ﻿using System;
 
-namespace EmployeePresence
+namespace employeeWage
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //constants
+            //Contant
             int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
+            int EMP_RATE_PER_HOUR = 20;
+            //Variable
+            int empHrs = 0;
+            int empWage = 0;
+            int totalEmpWage = 0;
             Random random = new Random();
             //Computation
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_PART_TIME)
             {
-                Console.WriteLine("Employee is Present");
+                empHrs = 8;
+            }
+            else if (empCheck == IS_FULL_TIME)
+            {
+                empHrs = 8;
             }
             else
             {
-                Console.WriteLine("Employee is Absent");
+                empHrs = 0;
             }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage : " + empWage);
         }
     }
 }
