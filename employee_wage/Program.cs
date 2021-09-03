599a7041c337fd004aa6ee675f5186d35f8f6445
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-09-03 21:53:05 +0530

    employee attendance

--- employee_wage/Program.cs
+++ employee_wage/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 
-namespace employee_wage
+namespace EmployeePresence
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //constants
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            //Computation
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
 }
