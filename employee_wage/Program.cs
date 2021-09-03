43d35e22a44dc0ec23b88d66397e7c2b0cfdeab7
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-09-03 22:17:50 +0530

    conditon

--- employee_wage/Program.cs
+++ employee_wage/Program.cs
@@ -9,15 +9,17 @@ namespace employeeWage
         public const int IS_PART_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
         public const int NUM_OF_WORKING_DAYS = 20;
+        public const int MAX_HRS_IN_MONTH = 100;
 
         static void Main(string[] args)
         {
             //Variable
             int empHrs = 0;
-            int empWage = 0;
-            int totalEmpWage = 0;
-            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+            int totalWorkingDays = 0;
+            int totalEmpHr = 0;
+            while (totalEmpHr <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
             {
+                totalWorkingDays++;
                 Random random = new Random();
                 //Computation
                 int empCheck = random.Next(0, 2);
@@ -34,10 +36,10 @@ namespace employeeWage
                         empHrs = 0;
                         break;
                 }
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                totalEmpWage += empWage;
-                Console.WriteLine("Emp Wage : " + empWage);
+                totalEmpHr += empHrs;
+                Console.WriteLine("Days : " + totalWorkingDays + "       " + "Emp Hrs : " + empHrs);
             }
+            int totalEmpWage = totalEmpHr * EMP_RATE_PER_HOUR;
             Console.WriteLine("Total Emp Wage : " + totalEmpWage);
         }
     }
