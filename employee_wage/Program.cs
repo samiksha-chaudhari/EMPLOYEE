456d54dfc7e8424835566c9bdbc400a59c0d4c27
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-09-03 22:15:19 +0530

    month wage

--- employee_wage/Program.cs
+++ employee_wage/Program.cs
@@ -8,6 +8,7 @@ namespace employeeWage
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
 
         static void Main(string[] args)
         {
@@ -15,24 +16,29 @@ namespace employeeWage
             int empHrs = 0;
             int empWage = 0;
             int totalEmpWage = 0;
-            Random random = new Random();
-            //Computation
-            int empCheck = random.Next(0, 2);
-
-            switch (empCheck)
+            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
             {
-                case IS_PART_TIME:
-                    empHrs = 8;
-                    break;
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                Random random = new Random();
+                //Computation
+                int empCheck = random.Next(0, 2);
+
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 8;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("Emp Wage : " + empWage);
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp Wage : " + empWage);
+            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
         }
     }
 }
