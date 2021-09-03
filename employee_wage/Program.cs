d4ca1c5ef06d8752a17c395f4bbc8e7da331cb14
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-09-03 22:11:53 +0530

    employee wage by switch

--- employee_wage/Program.cs
+++ employee_wage/Program.cs
@@ -1,24 +1,38 @@
 ﻿using System;
 
-namespace EmployeePresence
+namespace employeeWage
 {
     class Program
     {
+        //Contant
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+
         static void Main(string[] args)
         {
-            //constants
-            int IS_FULL_TIME = 1;
+            //Variable
+            int empHrs = 0;
+            int empWage = 0;
+            int totalEmpWage = 0;
             Random random = new Random();
             //Computation
             int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
+
+            switch (empCheck)
             {
-                Console.WriteLine("Employee is Present");
+                case IS_PART_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
             }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
-            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage : " + empWage);
         }
     }
 }
